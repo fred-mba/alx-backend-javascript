9ebd9946b572086826b3dbacf94c494f63d4b16e
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-07-03 23:05:15 +0300

    Append three handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,21 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  const myPromise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: promise,
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
-  return myPromise
+  return promise
     // when resolved, return the response object
-    .then((response) => response)
+    .then(() => ({ status: 200, body: 'success' })
     // when rejected return an empty Error object
     .catch(() => new Error())
     // log the message to the console for every resolution
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .finally(() => console.log('Got a response from the API'));
 }
