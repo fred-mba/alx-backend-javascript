792130b0657da5948c0ab9a075c69ae0cd82afdf
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-07-04 23:34:38 +0300

    New promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
 export default function getResponseFromAPI() {
-  const apiPromise = new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
-      resolve('Data fetched successfully.');
-    } else {
-      reject(new Error('Failed to fetch data.'));
-    }
-  });
-  return apiPromise;
+  return new Promise(() => {});
 }
