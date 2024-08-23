bd517af9cdc39ac4abedf9989b3b213db7bdc65b
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-08-24 00:05:48 +0300

    HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+/**
+ * A HTTP server using Express
+ * Should be assigned to the variable app and this one must be exported
+ * HTTP server should listen on port 1245
+ * Displays Hello Holberton School! in the page body for the endpoint /
+ */
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.listen(1245);
